eac09595db068b9f2a81e2de51adcb8c85dfaf26
Deivid <tucorreo@ejemplo.com>
2026-06-21 18:17:26 +0200

    add evaluacion

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const port = 5501;
 
-app.use(express.static("public"));
+app.use(express.static("public)"));
+
 app.get("/", (req, res) => {
   res.status(200).sendFile(__dirname + "/public/index.html");
 });
+
 app.get("/contact", (req, res) => {
   res.status(200).sendFile(__dirname + "/public/contact.html");
 });
